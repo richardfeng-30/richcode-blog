8fd55bd2ad805b42b596b9cc3ebbbc7744257b3a
richardfeng-30 <richardqfeng@gmail.com>
2024-01-14 19:48:06 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <script
+        defer
+        src="https://us.umami.is/script.js"
+        data-website-id="7df07cb0-384d-4d94-a549-df04929513d9"
+      ></script>
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
