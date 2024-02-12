cc5b8a6f0cc46b111a49ffa8a22cb24bb9233e39
Mauricio <mauricio.macias2535@gmail.com>
2024-02-11 19:30:45 -0800

    added login

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import { NextResponse } from 'next/server'
-
-export const POST = async (req: Request, res: Response) => {
-  try {
-    const body = await req.json()
-    const { username, password } = body
-    if (username && password) {
-      // we need to connect to mysql
-      // and check if they are in the db
-      return NextResponse.json({ success: true, new: req })
-=======
 
 
 import { connect } from "lib/db";
@@ -39,28 +27,5 @@ export const POST = async (req: Request, res: Response) => {
         return NextResponse.json({success: false, users: 'username and password required'}, {status: 400});
     } catch (err) {
         return NextResponse.json({success: false, error: err}, {status: 400})
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-    }
-    return NextResponse.json({ success: false, new: req })
-  } catch (err) {
-    return NextResponse.json({ success: false, error: true })
-  }
-}
-<<<<<<< Updated upstream
-
-// CREATE TABLE `user` (
-//      `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-//      `userName` varchar(32) DEFAULT NULL,
-//      `password` varchar(32) DEFAULT NULL,
-//      `name` varchar(32) DEFAULT NULL,
-//      `school` varchar(32) DEFAULT NULL,
-//      `age` tinyint(100) DEFAULT NULL,
-//      PRIMARY KEY (`id`)
-//     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
->>>>>>> Stashed changes
-=======
     }
 }
->>>>>>> Stashed changes
